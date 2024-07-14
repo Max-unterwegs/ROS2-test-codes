4cbc9666abcd8e70ed05d1c2113d71fed875aed6
ryc-123 <2314676658@qq.com>
2024-07-14 22:43:54 +0800

    调参

--- detect_sign/detect_sign/detect_sign.py
+++ detect_sign/detect_sign/detect_sign.py
@@ -233,10 +233,10 @@ class DetectSign(Node):
                     os.system('ros2 action send_goal /rotate_angle irobot_create_msgs/action/RotateAngle "{angle: -1.57,max_rotation_speed: 0.9}"')
                     # os.system('sleep 2')
                     # 向前走一段距离
-                    # os.system('ros2 action send_goal /drive_distance irobot_create_msgs/action/DriveDistance "{distance: 0.5,max_translation_speed: 1.0}"')
+                    os.system('ros2 action send_goal /drive_distance irobot_create_msgs/action/DriveDistance "{distance: 0.2,max_translation_speed: 1.0}"')
                     # os.system('sleep 2')
                     # 转个弯
-                    os.system('ros2 action send_goal /drive_arc irobot_create_msgs/action/DriveArc "{angle: 1.57,radius: 0.4,translate_direction: 1,max_translation_speed: 1.0}"')
+                    os.system('ros2 action send_goal /drive_arc irobot_create_msgs/action/DriveArc "{angle: 1.57,radius: 0.3,translate_direction: 1,max_translation_speed: 1.0}"')
                     msg_pub_max_vel.data = 0.20
                     self.pub_max_vel.publish(msg_pub_max_vel)
 
